6778592606822f9c4741a6b19764f328e9c31569
Alex <alex.maccabi@gmail.com>
2015-12-17 14:37:41 +0200

    Update Build.java

--- src/main/java/com/speedledger/measure/jenkins/Build.java
+++ src/main/java/com/speedledger/measure/jenkins/Build.java
@@ -14,7 +14,7 @@ public class Build {
     public transient static final DateFormat DATE_FORMATTER = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssZ");
     //private static final String node_env = System.getProperty("NODE_ENV");
    // private String JENKINS_BUILD_URL = System.getProperty("jenkins.buildUrl");
-    private String NODE_ENV = System.getProperty("NODE_ENV");
+    private String NODE_ENV = System.getProperty("NODE_ENV", "kandy_prp");
     private String timestamp;
     private int number;
     private String jobName;
@@ -43,7 +43,6 @@ public class Build {
 // }
 
     public String getNode_env() {
-        Sytem.out.println(NODE_ENV);
         return NODE_ENV;
     }
     
