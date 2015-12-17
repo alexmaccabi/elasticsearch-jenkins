383f84605fb86d9b50ec7f891ba0f6b3b29a147a
Alex <alex.maccabi@gmail.com>
2015-12-17 14:46:05 +0200

    Update BuildListener.java

--- src/main/java/com/speedledger/measure/jenkins/BuildListener.java
+++ src/main/java/com/speedledger/measure/jenkins/BuildListener.java
@@ -119,7 +119,7 @@ public class BuildListener extends RunListener<Run> {
         //build.setEnvironment(environment);
         build.setTimestamp(run.getTimestamp());
         //build.setUrl(run.getUrl());
-        build.setNode_env(job.getNode_env());
+        build.setNode_env(run.getNode_env());
         
 /*        try { 
         //build.setNode_env(run.getNode_env());
