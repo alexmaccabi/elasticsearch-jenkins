556bb8453c9898a808129afc1de12a5c94cac2fa
Alex <alex.maccabi@gmail.com>
2015-12-17 14:30:21 +0200

    Update BuildListener.java

--- src/main/java/com/speedledger/measure/jenkins/BuildListener.java
+++ src/main/java/com/speedledger/measure/jenkins/BuildListener.java
@@ -25,8 +25,6 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 
 /**
